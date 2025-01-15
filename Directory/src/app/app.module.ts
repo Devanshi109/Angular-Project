b4e4ce052aead47deeea11ae538159540b65bbb0
Devanshi109 <devanshidesai109@gmail.com>
2025-01-15 16:19:29 -0500

    cvs-directory

---
+++ Directory/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DirectoryUsersComponent } from './components/directory-users/directory-users.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DirectoryUsersComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
