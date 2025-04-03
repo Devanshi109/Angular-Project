967083b3432766e7de4d9937426636e870444c62
Devanshi109 <devanshidesai109@gmail.com>
2025-04-03 13:09:46 -0400

    undoable counter

--- counter/src/app/app.component.ts
+++ counter/src/app/app.component.ts
@@ -7,38 +7,43 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  count = 0;
-  undo_arr: number[] = [];
-  redo_arr: number[] = [];
+  count: number = 0;
   history: string[] = [];
+  undo_arr: { ur: string, value: number }[] = [];
+  redo_arr: { ur: string, value: number }[] = [];
 
-  countChange(value: number) {
+  countChange(value: number): void {
     const lastCount = this.count;
     this.count += value;
-    
-    this.history.unshift(`${value > 0 ? '+' : ''}${value} (${lastCount} -> ${this.count})`);
-    this.undo_arr.push(value);
+
+    const ur = value > 0 ? `+${value}` : `${value}`;
+    this.history.unshift(`${ur} (${lastCount} -> ${this.count})`);
+
+    this.undo_arr.push({ ur, value });
     this.redo_arr = [];
-    
-    if (this.history.length > 50) {
+
+    if (this.undo_arr.length > 50) {
+      this.undo_arr.shift();
       this.history.pop();
-    } 
+    }
   }
 
   undo() {
     if (!this.undo_arr.length) return;
-    
-    const value = this.undo_arr.pop()!;
-    this.count -= value;
-    this.redo_arr.push(value);
+
+    const lastAction = this.undo_arr.pop()!;
+    this.count -= lastAction.value;
+    this.redo_arr.push(lastAction);
     this.history.shift();
   }
 
   redo() {
     if (!this.redo_arr.length) return;
-    
-    const value = this.redo_arr.pop()!;
-    this.countChange(value);
+
+    const lastRedo = this.redo_arr.pop()!;
+    this.count += lastRedo.value;
+    this.undo_arr.push(lastRedo);
+    this.history.unshift(`${lastRedo.ur} (${this.count - lastRedo.value} -> ${this.count})`)
   }
 
 }
